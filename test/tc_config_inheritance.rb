1328c780df6a0a59434a75fa9fe902b5338e2f06
weshatheleopard <github@wesha.name>
2013-11-04 20:24:40 -0600

    Adding tests

---
+++ test/tc_config_inheritance.rb
@@ -0,0 +1,42 @@
+#--
+#     This file is part of the X12Parser library that provides tools to
+#     manipulate X12 messages using Ruby native syntax.
+#
+#     http://x12parser.rubyforge.org 
+#     
+#     Copyright (C) 2008 APP Design, Inc.
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License as published by the Free Software Foundation; either
+#     version 2.1 of the License, or (at your option) any later version.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#++
+#
+require 'X12'
+require 'test/unit'
+
+class TestParserInheritance < Test::Unit::TestCase
+
+  def setup
+    # Nothing
+  end # setup
+
+  def teardown
+    # Nothing
+  end # teardown
+
+  def test_ST
+    parser = X12::Parser.new('test/config_inheritance.xml')
+    parser.factory('inheritance_test')
+  end # test_ST
+
+end # TestList
